8cb245fd05ebfdb16ea1015298ff8485b0e62ae5
hanokjoo <okjoo.han88@gmail.com>
2024-12-27 15:27:12 +0900

    여려 JSX 슬롯 활용법

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Section from "./Section";
 import TabButton from "./TabButton";
+import Tabs from "./Tabs";
 import { EXAMPLES } from "../data";
 
 export default function Examples() {
@@ -26,33 +27,38 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-            <menu>
-                <TabButton
-                    isSelected={selectedTopic === "components"}
-                    onClick={() => clickHandler("components")}
-                >
-                    Component
-                </TabButton>
-                <TabButton
-                    isSelected={selectedTopic === "jsx"}
-                    onClick={() => clickHandler("jsx")}
-                >
-                    JSX
-                </TabButton>
-                <TabButton
-                    isSelected={selectedTopic === "props"}
-                    onClick={() => clickHandler("props")}
-                >
-                    Props
-                </TabButton>
-                <TabButton
-                    isSelected={selectedTopic === "state"}
-                    onClick={() => clickHandler("state")}
-                >
-                    State
-                </TabButton>
-            </menu>
-            {tabContent}
+            <Tabs
+                buttons={
+                    <>
+                        <TabButton
+                            isSelected={selectedTopic === "components"}
+                            onClick={() => clickHandler("components")}
+                        >
+                            Component
+                        </TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === "jsx"}
+                            onClick={() => clickHandler("jsx")}
+                        >
+                            JSX
+                        </TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === "props"}
+                            onClick={() => clickHandler("props")}
+                        >
+                            Props
+                        </TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === "state"}
+                            onClick={() => clickHandler("state")}
+                        >
+                            State
+                        </TabButton>
+                    </>
+                }
+            >
+                {tabContent}
+            </Tabs>
         </Section>
     );
 }
