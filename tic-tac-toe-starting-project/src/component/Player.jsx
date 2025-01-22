53362ae6a147a86e9f64fedfd1fe97368819e705
hanokjoo <okjoo.han88@gmail.com>
2025-01-22 17:26:52 +0900

    사용자 입력 & 양방향 바인딩

--- tic-tac-toe-starting-project/src/component/Player.jsx
+++ tic-tac-toe-starting-project/src/component/Player.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
     function handleEditingClick() {
         setIsEditing((isEditing) => !isEditing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        console.log("event:", event);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        editablePlayerName = (
+            <input
+                type="text"
+                required
+                value={playerName}
+                onChange={handleChange}
+            />
+        );
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditingClick}>
