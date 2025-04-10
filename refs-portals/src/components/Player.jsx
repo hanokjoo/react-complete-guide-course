fc863efe2c3701565cc897fb71707b2a890e7018
hanokjoo <okjoo.han88@gmail.com>
2025-04-10 15:37:03 +0900

    Refs(참조)로 HTML 요소 연결 및 접근

--- refs-portals/src/components/Player.jsx
+++ refs-portals/src/components/Player.jsx
@@ -1,27 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
     const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-    const [submitted, setSubmitted] = useState(false);
-
-    function handleChange(event) {
-        setSubmitted(false);
-        setEnteredPlayerName(event.target.value);
-    }
+    const playerName = useRef();
 
     function handleClick() {
+        console.log(playerName.current.value);
+        setEnteredPlayerName(playerName.current.value);
         setSubmitted(true);
     }
 
     return (
         <section id="player">
-            <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+            <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
             <p>
-                <input
-                    type="text"
-                    value={enteredPlayerName}
-                    onChange={handleChange}
-                />
+                <input ref={playerName} type="text" />
                 <button onClick={handleClick}>Set Name</button>
             </p>
         </section>
